8729cbb5cda3e6bbe5ed72eda8449baa77242bf9
RameshNR <47139284+RameshNR@users.noreply.github.com>
2019-03-08 10:34:30 +0530

    Hibernate Demo

---
+++ HibernateDemo/src/HibernateDemo.java
@@ -0,0 +1,33 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateDemo {
+
+	public static void main(String arg[])
+	{
+	
+	Configuration config=new Configuration();
+	config.configure("hibernate.cfg.xml");
+	
+	SessionFactory sessionFactory=config.buildSessionFactory();
+	Session session=sessionFactory.openSession();
+	Transaction transaction=session.beginTransaction();
+	
+	com.Product product=new com.Product();
+	
+	product.setProductId(1020);
+	product.setProductName("Samsung 9+");
+	product.setPrice(7000);
+	
+	session.save(product);
+	
+	transaction.commit();
+	
+	session.close();
+	
+	System.out.println("Object Saved in Database");
+	
+	}
+}
---
+++ HibernateDemo/src/com/Product.java
@@ -0,0 +1,32 @@
+package com;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+public class Product {
+
+	
+	int productId;
+	String productName;
+	int price;
+	
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+}
